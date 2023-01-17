b42760e7526b01aca3d473cbd4e021edbd6d9c78
isaac <isaactonyloi@gmail.com>
2023-01-17 10:59:04 +0300

    react state hook arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,24 +3,48 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+  console.log(foods);
+  
+  function removeFood(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() =>removeFood(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+
+
+  function handleAddFood() {
+    const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    console.log(newFoodArray);
+      }
+
+
+  
+  
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
   );
+
+
 }
 
 export default SpicyFoodList;
